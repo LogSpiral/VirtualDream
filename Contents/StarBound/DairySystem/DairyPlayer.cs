fb78a7d78cfc38928b429e9c44a3a2b8d70b3c5d
向内延伸的对数螺线 <3253615697@qq.com>
2022-12-26 13:30:32 +0800

    初步添加了点东西

---
+++ Contents/StarBound/DairySystem/DairyPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader.IO;
+
+namespace VirtualDream.Contents.StarBound.DairySystem
+{
+    public class DairyPlayer : ModPlayer
+    {
+        public override void LoadData(TagCompound tag)
+        {
+            base.LoadData(tag);
+        }
+        public override void SaveData(TagCompound tag)
+        {
+            base.SaveData(tag);
+        }
+    }
+}
---
+++ Contents/StarBound/SaveModify/SaveModifySystem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader.IO;
+
+namespace VirtualDream.Contents.StarBound.SaveModify
+{
+    public class SaveModifySystem : ModSystem
+    {
+        public override void SaveWorldData(TagCompound tag)
+        {
+            #region Player
+
+            #endregion
+        }
+        public override void LoadWorldData(TagCompound tag)
+        {
+            if (tag.TryGet("uniqueID", out int id))
+            {
+                uniqueID = id;
+            }
+            else 
+            {
+                uniqueID = Main.rand.Next(int.MaxValue);
+            }
+        }
+
+        public override void OnWorldLoad()
+        {
+            uniqueID = -1;
+        }
+
+        public override void OnWorldUnload()
+        {
+            uniqueID = -1;
+        }
+        internal int uniqueID;
+    }
+    public class SaveModifyPlayer : ModPlayer
+    {
+        internal int uniqueID = -1;
+        public override void SaveData(TagCompound tag)
+        {
+            tag.Add("position", Player.position);
+            tag.Add("velocity", Player.velocity);
+            tag.Add("Life", Player.statLife);
+            tag.Add("Mana", Player.statMana);
+            tag.Add("uniqueID", uniqueID);
+        }
+        public override void LoadData(TagCompound tag)
+        {
+            Player.position = tag.Get<Vector2>("position");
+            Player.velocity = tag.Get<Vector2>("velocity");
+            Player.statLife = tag.Get<int>("Life");
+            Player.statMana = tag.Get<int>("Mana");
+            uniqueID = tag.Get<int>("uniqueID");
+        }
+    }
+}
